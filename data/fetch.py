7733e828bb76820d446cb0379dfc3f2483d8e204
bekhzod-olimov <bekhzod.olimov@gmail.com>
2025-04-24 16:58:45 +0900

    initial commit

---
+++ data/fetch.py
@@ -0,0 +1,57 @@
+import os
+import shutil
+
+class DatasetDownloader:
+    def __init__(self, save_dir):
+        self.save_dir = save_dir
+        self.available_datasets = {
+            "pet_disease": "kaggle datasets download smadive/pet-disease-images",
+            "cars_detection": "kaggle datasets download -d killa92/car-detect-dataset",
+            "car_brands": "kaggle datasets download mohamedaziz15/cars-brands-in-egypt",
+            "apple_disease": "kaggle datasets download -d killa92/apple-disease-dataset",
+            "insects": "kaggle datasets download -d killa92/arthropodia-semantic-segmentation-dataset",
+            "crop": "kaggle datasets download -d killa92/crop-disease-image-classification-dataset",
+            "dog_breeds": "kaggle datasets download kabilan03/dogbreedclassification",
+            "rice_leaf_disease": "kaggle datasets download -d killa92/rice-leaf-disease-classification-dataset",
+            "lentils": "kaggle datasets download -d killa92/lentils-classification-dataset",
+            "brain": "kaggle datasets download -d killa92/brain-ct-tumor-classification-dataset",
+            "fruits": "kaggle datasets download -d killa92/fruits-classification-dataset",
+            "aeroscapes": "kaggle datasets download -d killa92/aerospaces-semantic-segmentation-dataset",
+            "cardiac": "kaggle datasets download -d killa92/cardiac-semantic-segmentation-dataset",
+            "vehicles": "kaggle datasets download -d killa92/vehicle-detection-dataset",
+            "cloud": "kaggle datasets download -d killa92/cloud-quality-images-classification-dataset",
+            "skin_lesion": "kaggle datasets download -d killa92/skin-lesion-image-ham-classification",
+            "comments": "kaggle datasets download -d killa92/comments-nlp-sentiment-analysis-dataset",
+            "3d_data": "kaggle datasets download glxslzt/3cla-adni",
+            "50_car_brands": "kaggle datasets download yamaerenay/100-images-of-top-50-car-brands"
+        }
+
+    def download(self, ds_nomi="lentils"):
+        assert ds_nomi in self.available_datasets, f"Mavjud bo'lgan datasetlardan birini tanlang: {list(self.available_datasets.keys())}"
+
+        dataset_path = os.path.join(self.save_dir, ds_nomi)
+        if os.path.isfile(f"{dataset_path}.csv") or os.path.isdir(dataset_path):
+            print(f"Dataset allaqachon mavjud: {dataset_path}")
+            return
+
+        url = self.available_datasets[ds_nomi]
+        if not url:
+            print(f"{ds_nomi} dataset uchun yuklab olish manzili yo'q.")
+            return
+
+        dataset_folder_name = url.split("/")[-1]
+        full_path = os.path.join(self.save_dir, dataset_folder_name)
+
+        print(f"{ds_nomi} dataset yuklanmoqda...")
+        os.system(f"{url} -p {full_path}")
+
+        archive_path = os.path.join(full_path, f"{dataset_folder_name}.zip")
+        extracted_path = os.path.join(self.save_dir, dataset_folder_name)
+
+        if os.path.exists(archive_path):
+            shutil.unpack_archive(archive_path, extracted_path)
+            os.remove(archive_path)
+            os.rename(extracted_path, dataset_path)
+            print(f"{ds_nomi} dataset '{dataset_path}' ga muvaffaqiyatli yuklandi!")
+        else:
+            print("Arxiv fayl topilmadi, ehtimol yuklab olish muvaffaqiyatsiz bo'lgan.")
---
+++ data/parse.py
@@ -0,0 +1,77 @@
+import os
+import torch
+import numpy as np
+from glob import glob
+from PIL import Image
+from torch.utils.data import random_split, Dataset, DataLoader
+
+torch.manual_seed(2025)
+
+class CustomDataset(Dataset):
+    def __init__(self, data_turgan_yolak, ds_nomi, tfs=None, im_files=[".png", ".jpg", ".jpeg", ".bmp"]):
+        
+        self.tfs, self.ds_nomi = tfs, ds_nomi
+        self.data_turgan_yolak = data_turgan_yolak 
+        self.im_files          = im_files      
+
+        self.get_root(); self.get_files(); self.get_info()                
+
+    def get_root(self): 
+        if self.ds_nomi == "pet_disease": self.root = f"{self.data_turgan_yolak}/{self.ds_nomi}/{self.ds_nomi}/data"
+        if self.ds_nomi == "lentils": self.root = f"{self.data_turgan_yolak}/{self.ds_nomi}/lentils/data"        
+        elif self.ds_nomi == "rice_leaf_disease": self.root = f"{self.data_turgan_yolak}/{self.ds_nomi}/rice_leaf_disease/Rice Leaf  Disease Dataset"
+        elif self.ds_nomi == "car_brands": self.root = f"{self.data_turgan_yolak}/{self.ds_nomi}/car_brands"
+        elif self.ds_nomi == "dog_breeds": self.root = f"{self.data_turgan_yolak}/{self.ds_nomi}/dog_breeds/Dog Breed Classification"        
+    
+    def get_files(self): 
+        if self.ds_nomi in ["pet_disease", "rice_leaf_disease", "car_brands"]: self.im_paths = [path for im_file in self.im_files for path in glob(f"{self.root}/*/*{im_file}")]        
+        elif self.ds_nomi in ["dog_breeds"]: self.im_paths = [path for im_file in self.im_files for path in glob(f"{self.root}/*/*/*{im_file}")]
+        elif self.ds_nomi in ["lentils"]: self.im_paths = [path for im_file in self.im_files for path in glob(f"{self.root}/*{im_file}")]
+
+    def get_info(self):
+
+        self.cls_names, self.cls_counts = {}, {}
+        count = 0
+        for im_path in self.im_paths:
+            class_name = self.get_class(im_path)
+            if class_name not in self.cls_names:
+                self.cls_names[class_name] = count
+                self.cls_counts[class_name] = 1
+                count += 1
+            else: self.cls_counts[class_name] += 1
+    
+    def get_class(self, path): 
+        if self.ds_nomi == "lentils": return os.path.basename(path).split("_")[0]
+        else: return os.path.dirname(path).split("/")[-1]
+
+    def __len__(self): return len(self.im_paths)
+
+    def __getitem__(self, idx):
+        
+        im_path = self.im_paths[idx]
+        im = Image.open(im_path)
+        if im.mode != "RGB": im = im.convert("RGB")
+        
+        gt = self.cls_names[self.get_class(im_path)]
+
+        if self.tfs: im = self.tfs(im)
+
+        return im, gt
+
+    @classmethod
+    def get_dls(cls, data_turgan_yolak, ds_nomi, tfs, bs, split=[0.8, 0.1, 0.1], ns=4):
+        
+        ds = cls(data_turgan_yolak=data_turgan_yolak, ds_nomi=ds_nomi, tfs=tfs)
+
+        total_len = len(ds)
+        tr_len = int(total_len * split[0])
+        vl_len = int(total_len * split[1])
+        ts_len = total_len - (tr_len + vl_len)
+
+        tr_ds, vl_ds, ts_ds = random_split(ds, [tr_len, vl_len, ts_len])
+
+        tr_dl = DataLoader(tr_ds, batch_size=bs, shuffle=True, num_workers=ns)
+        val_dl = DataLoader(vl_ds, batch_size=bs, shuffle=False, num_workers=ns)
+        ts_dl = DataLoader(ts_ds, batch_size=1, shuffle=False, num_workers=ns)
+
+        return tr_dl, val_dl, ts_dl, ds.cls_names, ds.cls_counts
